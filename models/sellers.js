23940ccd550a4334b8c2fde8887ac8adc4687cbc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-23 12:42:47 +0100

    Add sellers model

---
+++ models/sellers.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+const SellersSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    image: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+  },
+  { timestamps: true }
+);
+
+SellersSchema.virtual("url").get(function () {
+  return `/sellers/${this._id}`;
+});
+
+module.exports = model("Sellers", SellersSchema);
