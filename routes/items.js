acd7eef6d1526dadb5995b1c8ae0ac33f0957b48
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-24 03:13:05 +0100

    add items route

---
+++ routes/items.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const router = express.Router();
+
+const itemController = require("../controllers/item");
+
+router.get("/", itemController.getItems);
+
+router.get("/:id", itemController.getItem);
+
+router.get("/create", itemController.getCreateItem);
+
+router.post("/create", itemController.postCreateItem);
+
+router.get("/:id/edit", itemController.getEditItem);
+
+router.post("/:id/edit", itemController.postEditItem);
+
+router.get("/:id/delete", itemController.getDeleteItem);
+
+router.post("/:id/delete", itemController.postDeleteItem);
+
+module.exports = router;
