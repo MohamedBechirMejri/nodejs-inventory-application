b009f8ef4a0bf97dd5d5581b653dda88b4e82fdc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-24 04:04:10 +0100

    destructure controller

--- routes/sellers.js
+++ routes/sellers.js
@@ -2,22 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
-const sellerController = require("../controllers/seller");
+const {
+  index,
+  getSeller,
+  getCreateSeller,
+  postCreateSeller,
+  getEditSeller,
+  postEditSeller,
+  getDeleteSeller,
+  postDeleteSeller,
+} = require("../controllers/seller");
 
-router.get("/", sellerController.index);
+router.get("/", index);
 
-router.get("/:id", sellerController.getSeller);
+router.get("/:id", getSeller);
 
-router.get("/create", sellerController.getCreateSeller);
+router.get("/create", getCreateSeller);
 
-router.post("/create", sellerController.postCreateSeller);
+router.post("/create", postCreateSeller);
 
-router.get("/:id/edit", sellerController.getEditSeller);
+router.get("/:id/edit", getEditSeller);
 
-router.post("/:id/edit", sellerController.postEditSeller);
+router.post("/:id/edit", postEditSeller);
 
-router.get("/:id/delete", sellerController.getDeleteSeller);
+router.get("/:id/delete", getDeleteSeller);
 
-router.post("/:id/delete", sellerController.postDeleteSeller);
+router.post("/:id/delete", postDeleteSeller);
 
 module.exports = router;
