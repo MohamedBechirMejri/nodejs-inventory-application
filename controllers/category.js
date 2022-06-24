d43eebb9cd56e40db4bc4a3c2968b477df06fbb6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-24 04:19:57 +0100

    add get category method

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,9 @@
+/* eslint-disable consistent-return */
 const debug = require("debug")("app:category");
+const async = require("async");
+
 const Category = require("../models/category");
+const Item = require("../models/item");
 
 exports.index = (req, res) => {
   Category.find()
@@ -9,4 +13,32 @@ exports.index = (req, res) => {
     .catch(err => {
       debug(err);
     });
+};
+
+exports.getCategory = (req, res, next) => {
+  async.parallel(
+    {
+      category: callback => {
+        Category.findById(req.params.id).exec(callback);
+      },
+      items: callback => {
+        Item.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.category === null) {
+        const error = new Error("Category not found");
+        error.status = 404;
+        return next(error);
+      }
+
+      res.render("categories/show", {
+        category: results.category,
+        items: results.items,
+      });
+    }
+  );
 };
