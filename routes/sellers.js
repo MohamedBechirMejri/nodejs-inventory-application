e0e83308726ddb475c82eac41c2ba0f8baec3a80
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-24 03:10:34 +0100

    add seller route

---
+++ routes/sellers.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const router = express.Router();
+
+const sellerController = require("../controllers/seller");
+
+router.get("/", sellerController.getSellers);
+
+router.get("/:id", sellerController.getSeller);
+
+router.get("/create", sellerController.getCreateSeller);
+
+router.post("/create", sellerController.postCreateSeller);
+
+router.get("/:id/edit", sellerController.getEditSeller);
+
+router.post("/:id/edit", sellerController.postEditSeller);
+
+router.get("/:id/delete", sellerController.getDeleteSeller);
+
+router.post("/:id/delete", sellerController.postDeleteSeller);
+
+module.exports = router;
