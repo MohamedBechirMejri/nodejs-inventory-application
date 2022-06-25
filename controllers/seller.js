d8c70911e4d7c6678a885de95ccfe223d03e68a2
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-25 08:43:25 +0100

    add read method

--- controllers/seller.js
+++ controllers/seller.js
@@ -1,5 +1,9 @@
+/* eslint-disable consistent-return */
 const debug = require("debug")("app:seller");
+const async = require("async");
+
 const Seller = require("../models/seller");
+const Item = require("../models/item");
 
 exports.index = (req, res) => {
   Seller.find()
@@ -9,4 +13,32 @@ exports.index = (req, res) => {
     .catch(err => {
       debug(err);
     });
+};
+
+exports.read = (req, res, next) => {
+  async.parallel(
+    {
+      seller: callback => {
+        Seller.findById(req.params.id).exec(callback);
+      },
+      items: callback => {
+        Item.find({ seller: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.seller === null) {
+        const error = new Error("Seller not found");
+        error.status = 404;
+        return next(error);
+      }
+
+      res.render("sellers/show", {
+        seller: results.seller,
+        items: results.items,
+      });
+    }
+  );
 };
