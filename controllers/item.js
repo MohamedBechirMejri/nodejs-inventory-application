4d1d1c96bbce7e492d6bc049103a1fa081715caf
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-26 08:40:45 +0100

    add read method

--- controllers/item.js
+++ controllers/item.js
@@ -1,5 +1,10 @@
+/* eslint-disable consistent-return */
 const debug = require("debug")("app:item");
+const async = require("async");
+
 const Item = require("../models/item");
+const Seller = require("../models/seller");
+const Category = require("../models/category");
 
 exports.index = (req, res) => {
   Item.find()
@@ -9,4 +14,27 @@ exports.index = (req, res) => {
     .catch(err => {
       debug(err);
     });
+};
+
+exports.read = (req, res, next) => {
+  async.parallel(
+    {
+      item: callback => {
+        Item.findById(req.params.id).populate("seller category").exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.item === null) {
+        const error = new Error("Item not found");
+        error.status = 404;
+        return next(error);
+      }
+      res.render("items/show", {
+        item: results.item,
+      });
+    }
+  );
 };
