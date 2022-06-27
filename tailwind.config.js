88289e8cdb6c8fb3bb9108b2e94e7c9f99f2261e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-27 08:35:57 +0100

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+/* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.html", "./views/**/*.pug"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
